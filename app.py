c02e44da74d791936aeb4ad185e653af7145b535
MD Shoaib Ali <shoaib.ali@example.com>
2025-03-22 06:03:22 +0530

    mul added

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 def sum(a,b,c):
     return a+b+c
 
-<<<<<<< HEAD
 def sub(a,b,d):
     return a-b-d
-=======
-def sub(a,b,e):
-    return a-b-e
->>>>>>> 9e8f1923d670464048c49f7f401ed81bac8422b5
+
+def mul(a,b):
+    return a*b
 
 def division(a,b):
     return a/b
